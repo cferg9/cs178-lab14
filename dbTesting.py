b30c378f2f4381d448a1d273603fd87317b8b9e2
Colton <cmfergie9@gmail.com>
2026-03-26 11:02:14 -0500

    it works??

--- dbTesting.py
+++ dbTesting.py
@@ -36,8 +36,7 @@ rows = execute_query("""
     JOIN Track ON Album.AlbumId = Track.AlbumId
     LIMIT 10
 """)
-print(rows)
-#for row in rows:
-   # print(row[0], row[1], row[2], row[3], row[4])
+for row in rows:
+    print(row[0], row[1], row[2], row[3], row[4])
 
 # TODO: Exercise 1 — modify the query above to also return the Milliseconds column
